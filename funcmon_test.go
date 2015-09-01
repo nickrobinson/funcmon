db66ae5a9366a57d39d1df944eae90de72b65aae
Nick Robinson <nr33@msstate.edu>
2015-08-31 21:23:37 -0400

    Adding initial unit tests

---
+++ funcmon_test.go
@@ -0,0 +1,31 @@
+package funcmon
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+	"github.com/nickrobinson/funcmon"
+
+)
+
+func TestStub(t *testing.T) {
+    assert.True(t, true, "This is good. Canary test passing")
+}
+
+func TestNewConfig(t *testing.T) {
+	config := funcmon.NewConfig()
+	assert.Equal(t, config.Port, 8086)
+	assert.NotNil(t, config.Host, "Host field not present")
+	assert.NotNil(t, config.DB, "DB field not present")
+}
+
+func TestNewClient(t *testing.T) {
+	config := funcmon.Config{
+		Host: "127.0.0.1",
+		Port: 8086,
+		DB: "funcmon",
+	}
+	client,err := funcmon.NewClient(config)
+	assert.Nil(t, err, "Error is not nil")
+	assert.NotNil(t, client, "Client is not initialized")
+	
+}
